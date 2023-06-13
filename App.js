6f3ee1e59670e111f3fe1160b3c4dbc2c45dce3d
Mujahid1717 <mujahidakhtar2003@gmail.com>
2023-06-13 10:48:36 +0500

    applying RTk simple hitting api call

--- App.js
+++ App.js
@@ -1,51 +1,18 @@
-import { StyleSheet, Text, View, SafeAreaView, ActivityIndicator } from 'react-native';
-import React, { useEffect, useState } from 'react';
-
-const BASE_URL = 'https://jsonplaceholder.typicode.com/posts/1'
-
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import HomeScreen from './src/screens/HomeScreen'
+import {store} from './src/store/store'
+import { Provider } from 'react-redux'
 
 const App = () => {
-  const [loader,setLoader] = useState(true);
-  const [data,setData] = useState(null);
-  const [error,setError] = useState();
   
-
-  useEffect(()=>{
-    const apiCall = async () => {
-
-      try {
-      setLoader(true)
-      let response = await (await fetch(BASE_URL)).json()
-      console.log(response, 'responsssee')
-      setData(response)
-      setLoader(false)
-      }
-      catch{
-        setError(error)
-        setLoader(false)
-      }
-    }
-    apiCall()
-    
-  },[])
-  if (loader){
-    <View style = {{flex:1,alignContent:'center',justifyContent:'center'}}>
-      <ActivityIndicator color = 'black' size='large'></ActivityIndicator>
-    </View>
-  }
-
   return (
-    <SafeAreaView>
-    <View>
-      <Text style={{color:'black',fontSize:20,textAlign:'center',margin:20}}>{data?.title}</Text>
-    </View>
-    </SafeAreaView>
-  );
-};
+    <Provider store = {store}>
+     <HomeScreen />
+     </Provider>
+  )
+}
 
+export default App
 
-
-const styles = StyleSheet.create({
-
-});
-export default App;
+const styles = StyleSheet.create({})
